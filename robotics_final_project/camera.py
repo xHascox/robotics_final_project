752ad8a79a6ed7dd84a4aa733fdfdf50a320d421
xHascox <mg.marco@hotmail.ch>
2024-05-30 01:37:52 +0200

    camera working in package

---
+++ robotics_final_project/camera.py
@@ -0,0 +1,63 @@
+# type: ignore
+
+import time
+import patch_ftp
+import robomaster.config  # noqa
+from robomaster import robot, logger, logging  # noqa
+import robomaster.conn
+import robomaster.camera
+import cv2
+import argparse
+
+
+
+def get_image(robot, filename=False, resolution="720"):
+    robot.camera.start_video_stream(display=False, resolution=f"{resolution}p")
+    try:
+        frame = robot.camera.read_cv2_image()
+        print('Frames have shape', frame.shape)
+        if filename: cv2.imwrite(filename+".png", frame)
+        #cv2.imshow('image', frame)
+        
+        #cv2.waitKey(1)
+        robot.camera.stop_video_stream()
+        return True, frame
+    except Exception as e:
+        print(f"failed {e}")
+        return [False]
+
+def get_images(resolution=720, log_level="WARN", ):
+    # max resolution is 720p for video?
+
+    logger.setLevel(log_level)
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="sta")
+    time.sleep(1)
+
+    print('Camera version', ep_robot.camera.get_version())
+    print('Camera configuration', vars(ep_robot.camera.conf))
+    print('Camera address', ep_robot.camera.video_stream_addr)
+    print('Camera resolution', f"{resolution}p")
+    
+
+    while not get_image(ep_robot, "straight", 720)[0]:
+        time.sleep(1)
+    print("move gimbal")
+    ep_robot.gimbal.moveto(yaw=45, pitch=0, yaw_speed=90, pitch_speed=30).wait_for_completed()
+    print("moved gimbal")
+    #time.sleep(1)
+
+
+    while not get_image(ep_robot, "right_gimbal", 720)[0]:
+        time.sleep(1)
+    print("finished")
+
+    
+    #print("closed video")
+    #ep_robot.close()
+    print("closed robot")
+
+
+if __name__ == '__main__':
+    get_images()
+    
--- robotics_final_project/final_node.py
+++ robotics_final_project/final_node.py
@@ -1,3 +1,48 @@
+import camera
+
+import time
+import patch_ftp
+import robomaster.config  # noqa
+from robomaster import robot, logger, logging  # noqa
+import robomaster.conn
+import robomaster.camera
+import cv2
+
+
+
 
 def main():
-    print("ok")
+    print("ok v2")
+
+
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="sta")
+    time.sleep(1)
+
+    # get front view
+    success, frame = camera.get_image(ep_robot)
+    while not success:
+        time.sleep(1)
+        success, frame = camera.get_image(ep_robot)
+    #cv2.imshow(frame)
+    
+
+    # get right view
+    ep_robot.gimbal.moveto(yaw=45, pitch=0, yaw_speed=90, pitch_speed=30).wait_for_completed()
+    print("moved gimbal")
+    success2, frame2 = camera.get_image(ep_robot)
+    while not success:
+        time.sleep(1)
+        success2, frame2 = camera.get_image(ep_robot)
+    
+
+
+    # create map
+
+    # calculate path
+
+    # drive
+
+    print("finished")
+
+main()
